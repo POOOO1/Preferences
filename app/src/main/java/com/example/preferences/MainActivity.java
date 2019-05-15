7d175333cc83d067779e2e49a06935c28cbb8ad2
polina <p-leonova@list.ru>
2019-05-15 17:00:08 +0300

    contains(String key): возвращает true, если в настройках сохранено значение с ключом key
    getAll(): возвращает все сохраненные в настройках значения
    
    getBoolean (String key, boolean defValue): возвращает из настроек значение типа Boolean, которое имеет ключ key. Если элемента с таким ключом не окажется, то возвращается значение defValue, передаваемое вторым параметром
    
    getFloat(String key, float defValue): возвращает значение типа float с ключом key. Если элемента с таким ключом не окажется, то возвращается значение defValue
    
    getInt(String key, int defValue): возвращает значение типа int с ключом key
    
    getLong(String key, long defValue): возвращает значение типа long с ключом key
    
    getString(String key, String defValue): возвращает строковое значение с ключом key
    
    getStringSet(String key, Set<String> defValues): возвращает массив строк с ключом key
    
    edit(): возвращает объект SharedPreferences.Editor, который используется для редактирования настроек

--- app/src/main/java/com/example/preferences/MainActivity.java
+++ app/src/main/java/com/example/preferences/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.preferences;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREFS_FILE = "Account";
+    private static final String PREF_NAME = "Name";
+    SharedPreferences settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settings = getSharedPreferences(PREFS_FILE, MODE_PRIVATE);
+    }
+
+    public void saveName(View view) {
+        // получаем введенное имя
+        EditText nameBox = (EditText) findViewById(R.id.nameBox);
+        String name = nameBox.getText().toString();
+        // сохраняем его в настройках
+        SharedPreferences.Editor prefEditor = settings.edit();
+        prefEditor.putString(PREF_NAME, name);
+        prefEditor.apply();
+    }
+
+    public void getName(View view) {
+        // получаем сохраненное имя
+        TextView nameView = (TextView) findViewById(R.id.nameView);
+        String name = settings.getString(PREF_NAME,"не определено");
+        nameView.setText(name);
     }
 }
